c10b63609493ff894e3abe4a365b3cc6c87095de
Enrique Costa <enrique@computer.local>
2025-04-22 22:20:24 -0400

    Corregir variables de entorno MySQL

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 def get_connection():
     return pymysql.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
 
 
